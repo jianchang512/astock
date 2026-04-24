2f5dddb80f7318c1d3b40ea9abab3b2d9e503e8c
jameson512 <2867557054@qq.com>
2026-04-24 21:43:49 +0800

    up

--- page/script/gen_page.py
+++ page/script/gen_page.py
@@ -185,17 +185,20 @@ document.addEventListener("DOMContentLoaded", function() {{
 def csv_to_md_sim():
     
     csv_map = {
-        "sim_trade_result_top1.csv": "[500股]前 1 只股票",
-        "sim_trade_result_top3.csv": "[500股]前 3 只股票",
-        "sim_trade_result_top5.csv": "[500股]前 5 只股票",
-        "sim_trade_result_top10.csv": "[500股]前 10 只股票",
-        "sim_trade_result_compare.csv": "[500股]不同方式收益对比",
+        "sim_trade_result_top1.csv": "前 1 只股票[500股]",
+        "sim_trade_result_top1-1000.csv": "前 1 只股票[1000股]",
         
-        "sim_trade_result_top1-1000.csv": "[1000股]前 1 只股票",
-        "sim_trade_result_top3-1000.csv": "[1000股]前 3 只股票",
-        "sim_trade_result_top5-1000.csv": "[1000股]前 5 只股票",
-        "sim_trade_result_top10-1000.csv": "[1000股]前 10 只股票",
-        "sim_trade_result_compare-1000.csv": "[1000股]不同方式收益对比",
+        "sim_trade_result_top3.csv": "前 3 只股票[500股]",
+        "sim_trade_result_top3-1000.csv": "前 3 只股票[1000股]",
+        
+        "sim_trade_result_top5.csv": "前 5 只股票[500股]",
+        "sim_trade_result_top5-1000.csv": "前 5 只股票[1000股]",
+        
+        "sim_trade_result_top10.csv": "前 10 只股票[500股]",
+        "sim_trade_result_top10-1000.csv": "前 10 只股票[1000股]",
+        
+        "sim_trade_result_compare.csv": "收益对比[500股]",
+        "sim_trade_result_compare-1000.csv": "收益对比[1000股]",
     }
     for csvname,title in csv_map.items():
         csv_path=Path(f'{PROJECT_ROOT}/tests/{csvname}')
